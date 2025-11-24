2df8137863709d405b1be4c83c383f21347bf20c
Samir3200 <samirelorf@yahoo.fr>
2025-11-24 12:02:14 +0100

    schemas.ts

---
+++ Samir3200/public/drizzle.ts
@@ -0,0 +1,8 @@
+
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+const sql = neon(process.env.DATA_URL as string);
+
+export const data = drizzle(sql);
+
---
+++ Samir3200/public/schemas.ts
@@ -0,0 +1,28 @@
+import { pgTable, serial, text, date } from "drizzle-orm/pg-core";
+
+
+
+export const Frida = pgTable("Frida", {
+    id: serial("id").primaryKey(),
+    nomProjet: text("nomProjet").notNull(),
+});
+
+
+export const Promo = pgTable("Promo", {
+    id:serial("id").primaryKey(),
+    nomPromo:text("nomPromo").notNull(),
+    dateStart:date("dateStart").notNull()
+})
+
+
+export const studentProject = pgTable("studentProject", {
+    id:serial("id").primaryKey(),
+    title:text("text").notNull(),
+    image:text("image").notNull(),
+    PersonnalLink:text("PersonnalLink").notNull(),
+    DemoLink:text("Demolink").notNull(),
+    DateCreat:text("DateCreat").notNull(),
+    PublicDate:text("PublicDate").notNull(),
+    PromoLink:text("PromoLink").notNull(),
+    ProjetLink:text("ProjetLink").notNull()
+})
